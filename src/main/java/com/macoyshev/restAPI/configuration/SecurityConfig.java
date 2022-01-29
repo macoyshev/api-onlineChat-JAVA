f6cffdb5f01b395d51f154642b25a883bf9abcd1
macoyshev <make.oynoshev@gmail.com>
2022-01-29 23:18:32 +0300

    fix InMemoryUserDetails

--- src/main/java/com/macoyshev/restAPI/configuration/SecurityConfig.java
+++ src/main/java/com/macoyshev/restAPI/configuration/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.macoyshev.restAPI.configuration;
 
-import com.macoyshev.restAPI.security.services.MyUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,39 +11,37 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.client.HttpClientErrorException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private MyUserDetailService userDetailService;
-
-  //TODO: implement authEntryPoint
-  //private AuthEntryPointJwt unauthorized;
-
-  //TODO: finish configuration
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.cors().and().csrf().disable()
+    http
+      .csrf().disable()
       .authorizeRequests()
-      .exceptionHandling();
+      .antMatchers("/login").permitAll()
+      .antMatchers("/api/**").hasRole("ADMIN")
+      .anyRequest().authenticated()
+      .and()
+      .httpBasic();
   }
 
   @Override
   @Bean
   protected UserDetailsService userDetailsService() {
-    return new InMemoryUserDetailsManager(
-      User.builder()
-        .username("admin")
-        .password(passwordEncoder().encode("admin"))
-        .roles("ADMIN")
-        .build()
-    );
+    UserDetails user = User.builder()
+      .username("admin")
+      .password(passwordEncoder().encode("admin"))
+      .roles("ADMIN")
+      .build();
+
+    return new InMemoryUserDetailsManager(user);
   }
 
   @Bean
   protected PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder(12);
+    return new BCryptPasswordEncoder(10);
   }
 }
